7364a0e85ed57b911273156a01e9d6989ac92818
kaliiks <kaliik1@seznam.cz>
2018-02-17 17:17:48 +0100

    PHP Extensions/Requirements check + Removed support for Windows

--- BungeePM/src/BungeePE.php
+++ BungeePM/src/BungeePE.php
@@ -21,6 +21,18 @@ class BungeePE{
      */
     public function __construct(string $bindIp, int $bindPort, $password, $tickTimeout)
     {
+       if(PHP_VERSION_ID < 70200){
+           Logger::error("You must use PHP 7.2 or higher");
+           exit(127);
+       }
+       if(PHP_SHLIB_SUFFIX !== "dll"){
+           Logger::error("Use dll extensions and BungeeCord Custom build! Your build has invalid SHLIB_SUFFIX.");
+           exit(127);
+       }
+       if(strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN'){
+             Logger::error("BungeeCord support only Linux,CentOS & MacOS! FOR WINDOWS THERE IS MISSING SOCKET_UTILS EXTENSION");
+             exit(127);
+       }
        $this->socket = new BungeeUDPSocket($bindIp, $bindPort, $password);
     }
 
